a20cbd963ad6189969d6aefd8fd000a6ff472a04
artalar <artalarut@proton.me>
2023-07-12 08:27:13 +0300

    small refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-import '~/styles/globals.css'
-import { useMemo } from 'react'
 import type { AppProps } from 'next/app'
-import { createCtx, connectLogger } from '@reatom/framework'
-import { reatomContext } from '@reatom/npm-react'
+import { connectLogger } from '@reatom/framework'
+import { reatomContext, useCreateCtx } from '@reatom/npm-react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ctx = useMemo(() => {
-    const ctx = createCtx()
+  const ctx = useCreateCtx((ctx) => {
     if (typeof window !== undefined) connectLogger(ctx)
-    return ctx
-  }, [])
+  })
 
   return (
     <reatomContext.Provider value={ctx}>
